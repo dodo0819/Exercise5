7e1db550b6d01a60c45be226d79abb24d4372f29
dodo0819 <duwei583@gmail.com>
2019-12-28 21:49:10 +0800

    exercise5 doen

---
+++ app/src/main/java/com/example/exercise5/CountViewModel.kt
@@ -0,0 +1,18 @@
+package my.edu.tarc.exercise5
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class CountViewModel : ViewModel() {
+    var countLike: Int = 0
+    var countDislike: Int = 0
+
+    init {
+        Log.d("CounterViewModel","ViewModel created")
+    }
+
+    override fun onCleared() {
+        Log.d("CounterViewModel","ViewModel destroyed")
+        super.onCleared()
+    }
+}
